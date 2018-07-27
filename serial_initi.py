9ca74d9938b7d2e495b1296d8b1d25c97f02b8b7
mengzhangjian <mengzhangjian@gmail.com>
2018-07-27 13:01:41 +0800

    add tracking points

--- serial_initi.py
+++ serial_initi.py
@@ -6,14 +6,15 @@ import os
 import multiprocessing as mp
 from pydarknet import Detector, Image
 from collections import deque
-
+import time
 net = Detector(bytes("cfg_yolo/yolov3.cfg"), bytes("cfg_yolo/yolo-obj_60000.weights"), 0, bytes("cfg_yolo/coco.data"))
 index = [0]
 cmdl = []
 inbuf = []
-pts = deque(maxlen = 64)
+pts = deque(maxlen = 32)
 (dX,dY) = (0,0)
 direction = ""
+counter = 0
 
 avg = None
 for id in index:
@@ -34,6 +35,7 @@ for camID in index:
     cmdl[camID].CameraGetImageBufferSize(camID, byref(buflen), 0x4)
     inbuf.append(create_string_buffer(buflen.value))   
 
+time.sleep(2.0)
 while True:
     for camID in index:
         cmdl[camID].CameraQueryImage(camID,inbuf[camID],byref(buflen),0x104)
@@ -59,8 +61,37 @@ while True:
                 x,y,w,h = bounds
                 cv2.rectangle(img, (int(x - w / 2), int(y - h / 2)), (int(x + w / 2), int(y + h / 2)), (255, 0, 0), thickness=2)
                 cv2.putText(img,str(cat.decode("utf-8")),(int(x),int(y)),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,0))
+                center = (int(x),int(y))
+                pts.appendleft(center)
+        print(pts)
+        for i in np.arange(1, len(pts)):
+            
+            if pts[i - 1] is None or pts[i] is None:
+			    continue
+            if counter >= 10 and i == 1 and len(pts) > 5 and pts[-5] is not None:
+                dX = pts[-5][0] - pts[i][0]
+                dY = pts[-5][1] - pts[i][1]
+                (dirX,dirY) = ("","")
+
+                if np.abs(dX) > 20:
+                    dirX = "East" if np.sign(dX) == 1 else "West"
+                
+                if np.abs(dY) > 20:
+                    dirY = "North" if np.sign(dY) ==1 else "South"
+                
+                if dirX != "" and dirY != "":
+                    direction = "{}-{}".format(dirY,dirX)
+                else:
+                    direction = dirX if dirX !="" else dirY
+            thickness = int(np.sqrt(32 / float(i + 1)) * 2.5)
+            cv2.line(img, pts[i - 1], pts[i], (0, 0, 255), thickness)
+        cv2.putText(img, direction, (10, 30), cv2.FONT_HERSHEY_SIMPLEX,0.65, (0, 0, 255), 3)
+        cv2.putText(img, "dx: {}, dy: {}".format(dX, dY),
+		(10, img.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX,
+		0.35, (0, 0, 255), 1)
         cv2.imshow("Camera"+str(camID), img)
-        key = cv2.waitKey(2)
+        counter +=1
+        key = cv2.waitKey(1)
         if key & 0XFF == ord('q'):
             break
 cv2.destroyAllWindows()
